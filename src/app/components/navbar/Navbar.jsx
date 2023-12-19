9564bc0cee17eb5cce54185a007b042ea47c8a77
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-12-18 23:17:12 -0500

    update

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -1,17 +1,33 @@
-import Link from "next/link";
+import Link from 'next/link';
 import styles from '@/app/page.module.css';
 
 export default function Navbar() {
   return (
     <div className={styles.navContainer}>
-      <div className={styles.logo}>
-        <img src='/logo2.png' alt='logo'/></div>
+      <div className={styles.logoContainer}>
+        <Link className={styles.logoLink} href={'/'}>
+          <img className={styles.logo} src='/redditclonelogo.png' alt='logo' />
+        </Link>
+      </div>
       <div className={styles.navBar}>
-      <Link href={"/"}>Home</Link>
-      <Link href={"/subreddits"}>Subreddits</Link>
-      <Link href={"/login"}>Login</Link>
-      <Link href={"/register"}>Register</Link>
-      <Link href={"/logout"}>Logout</Link>
+        <Link className={styles.link} href={'/'}>
+          Home
+        </Link>
+        <Link className={styles.link} href={'/subreddits'}>
+          Subreddits
+        </Link>
+
+        <Link className={styles.link} href={'/logout'}>
+          Logout
+        </Link>
+      </div>
+      <div className={styles.navBtnContainer}>
+        <Link href='/login'>
+          <button className={styles.navBtn}>Login</button>
+        </Link>
+        <Link href='/register'>
+          <button className={styles.navBtn}>Register</button>
+        </Link>
       </div>
     </div>
   );

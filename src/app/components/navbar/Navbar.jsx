015acf560888529ee7b6f652b6f0b8f32f7b081f
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-12-18 21:29:37 -0500

    .

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
-import styles from '../page.module.css';
+import styles from '@/app/page.module.css';
 
 export default function Navbar() {
   return (
-    <div className={styles.navBar}>
+    <div className={styles.navContainer}>
+      <div className={styles.logo}>
+        <img src='/logo2.png' alt='logo'/></div>
+      <div className={styles.navBar}>
       <Link href={"/"}>Home</Link>
       <Link href={"/subreddits"}>Subreddits</Link>
       <Link href={"/login"}>Login</Link>
       <Link href={"/register"}>Register</Link>
       <Link href={"/logout"}>Logout</Link>
+      </div>
     </div>
   );
 }
